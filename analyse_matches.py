2d858923f067bc355a64a62bddce0318fda68a77
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2016-12-20 13:03:25 +0100

    Implemented partition in greedy matches and non-matches for analysis script

--- analyse_matches.py
+++ analyse_matches.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
+
 import math
 import numpy
 import os
 import sys
 
+threshold = 2.0
+
 def distance( a,b,c,d ):
     return math.sqrt( (a-c)**2 + (b-d)**2 )
 
 for filename in sys.argv[1:]:
-    name      = os.path.basename(filename)
+    name      = os.path.splitext( os.path.basename(filename) )[0]
     distances = list()
+    matches   = defaultdict(list)
 
     with open(filename) as f:
         for line in f:
-            (a,b,c,d) = [ float(x) for x in line.split() ]
+            (a,b,c,d) = [ int(x) for x in line.split() ]
+            matches[ (c,d) ].append( (a,b) )
             distances.append( distance(a,b,c,d) )
 
     print("Processed %s:" % filename )
@@ -23,3 +29,26 @@ for filename in sys.argv[1:]:
     print("  Median distance : %f" % numpy.median( distances ) )
     print("  Maximum distance: %f" % max( distances ) )
     print("  Quantile <= 2   : %f" % ( len( [ x for x in distances if x <= 2 ] ) / len(distances) ) )
+
+    # Sort potential assignments based on their distance to the source
+    # point. Afterwards, only the first entry is used to denote all of
+    # the greedy matches.
+    for c,d in sorted( matches.keys() ):
+        matches[(c,d)] = sorted( matches[(c,d)], key = lambda ab : distance(c,d,ab[0], ab[1] ) )
+
+    #
+    # Greedy matches
+    #
+    with open("/tmp/" + name + "_matched.txt", "w") as f:
+        for c,d in sorted( matches.keys() ):
+            a,b = matches[(c,d)][0]
+            print("%d\t%d\t%d\t%d" % (a,b,c,d ), file=f)
+
+    #
+    # Unmatched
+    #
+    with open("/tmp/" + name + "_unmatched.txt", "w") as f:
+        for c,d in sorted( matches.keys() ):
+            nonMatches = matches[(c,d)][1:]
+            for a,b in nonMatches:
+                print("%d\t%d" % (a,b), file=f)

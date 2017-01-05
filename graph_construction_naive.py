01d3d0dec1539bf022ff2a33bc0367f971a9387d
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-01-05 10:41:16 +0100

    Extended naive graph construction to partition graph
    So far, this only comprises connected components. Need a way to
    partition the graph into segments next. For this, I need to map
    the node degrees.

--- graph_construction_naive.py
+++ graph_construction_naive.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+#
+# Creates a graph from the raw skeleton data. The construction algorithm
+# is naive and merely checks for all neighbouring pixels, including the
+# diagonal one.
 
 import sys
 
@@ -7,6 +11,54 @@ coordinates = set()
 width  = 839 
 height = 396
 
+"""
+Union-find data structure for partitioning the graph into connected
+components segments, which may then subsequently be partitioned into
+individual segments.
+"""
+class UnionFind:
+    def __init__(self, vertices):
+        self.components = dict()
+
+        for vertex in vertices:
+            self.components[vertex] = vertex
+
+    """ Parent access with path compression """
+    def find(self, vertex):
+        if self.components[vertex] == vertex:
+            return vertex
+
+        # Path compression
+        else:
+            self.components[vertex] = self.find( self.components[vertex] )
+            return self.components[vertex]
+
+    """ Provides merging functionality """
+    def merge(self, u, v):
+        cu = self.find(u)
+        cv = self.find(v)
+
+        self.components[cu] = cv
+
+    """ Generator for all roots """
+    def roots(self):
+        for vertex in sorted( self.components.keys() ):
+            if self.find(vertex) == vertex:
+                yield vertex
+
+    """ Gets all vertices of a specific connected component """
+    def vertices(self, root):
+        result = list()
+
+        for vertex in self.components.keys():
+            if self.find(vertex) == root:
+                result.append( vertex )
+
+        return result
+
+"""
+Basic node class
+"""
 class Node:
     def __init__(self,x,y,i):
         self.x = x
@@ -33,10 +85,15 @@ def neighbours(x,y):
 with open(sys.argv[1]) as f:
     coordinates    = dict()
     edges          = list()
+    vertices       = set()
 
     for index,line in enumerate(f):
         (x,y)                   = [ int(a) for a in line.split() ]
         coordinates [ (x,y) ]   = index
+
+    #
+    # Graph creation
+    #
 
     for (x,y) in sorted( coordinates.keys() ):
         for (a,b) in neighbours(x,y):
@@ -44,8 +101,23 @@ with open(sys.argv[1]) as f:
                 u = coordinates[ (x,y) ]
                 v = coordinates[ (a,b) ]
 
+                vertices.add(u)
+                vertices.add(v)
+
                 if u < v:
                     edges.append( (u,v) )
+
+    #
+    # Calculate connected components
+    #
+
+    uf = UnionFind(vertices)
+
+    for (u,v) in edges:
+        uf.merge(u,v)
+
+    for root in uf.roots():
+        print(root, len(uf.vertices(root)))
 
 # TODO:
 # - Identify segments (union-find)

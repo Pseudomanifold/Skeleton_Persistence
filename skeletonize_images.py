109b3eb4c1df793d7e9281f4d05184a6ab961954
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-07-05 10:56:10 +0200

    Fixed skeletonization script
    * Automated output in '/tmp'
    * Fixed creation of skeletons based on thresholded image

--- skeletonize_images.py
+++ skeletonize_images.py
@@ -5,18 +5,25 @@ from skimage.filters    import threshold_otsu
 from skimage.morphology import skeletonize
 from skimage            import io
 
+import os
 import sys
 
 for filename in sys.argv[1:]:
     image    = io.imread(filename)
     image    = rgb2gray(image)
     binary   = image > threshold_otsu(image)
-    skeleton = skeletonize(image)
+    skeleton = skeletonize(binary)
+
+    output   = os.path.basename(filename)
+    output   = os.path.splitext(output)[0]
+    output   = "/tmp/" + output + ".txt"
 
     rows, columns = skeleton.shape
 
-    for row in range(rows):
-        for column in range(columns):
-            if skeleton[row,column]:
-                print(row,column)
+    print("Storing skeleton in '%s'" % output)
+    with open(output, "w") as f:
+        for row in range(rows):
+            for column in range(columns):
+                if skeleton[row,column]:
+                    print(row,column,file=f)
 
